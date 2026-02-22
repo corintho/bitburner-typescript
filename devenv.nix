70b747af21d2a8c25afc6b62d04a723aac02b47e
Corintho Assunção <github@corintho.eu>
2026-02-22 11:23:18 +0100

    chore(env): add devenv

---
+++ devenv.nix
@@ -0,0 +1,32 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = [ ];
+
+  # https://devenv.sh/languages/
+  languages.javascript = {
+    enable = true;
+    npm.enable = true;
+  };
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # See full reference at https://devenv.sh/reference/options/
+}
